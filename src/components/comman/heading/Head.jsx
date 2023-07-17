442caae5724642d6506588c3fc76998c855d6a90
ParasVsk <parasvasaikar159@gmail.com>
2023-07-17 20:09:43 +0530

    added blog and comman

---
+++ src/components/comman/heading/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const head = () => {
+  return (
+    <div>
+      <section className='head'>
+        <div className='container flexSB'>
+            <div className='logo'>
+                <h1>Directory Website</h1>
+            </div>
+            <div className="social">
+                <i className='fab crystal fa-facebook-f'></i>
+                <i className='fab fa-instagram'></i>
+                <i className='fab crystal fa-twitter'></i>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default head
